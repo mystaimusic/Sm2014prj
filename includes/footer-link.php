66b70db2a7ad2696915e010375c33e21cd2fa835
gabriele <mystaimusic@gmail.com>
2013-11-03 23:07:33 +0100

    Initial commit

---
+++ includes/footer-link.php
@@ -0,0 +1,8 @@
+    	<div class="footer_links"> 
+                <a href="index.php" title="">home</a>
+               <a href="multimedia.php" title="">multimedia</a>
+               <a href="editoria.php" title="">editoria</a>
+               <a href="object-design.php" title="">object design</a>
+              <a href="mailto:info@sintesilogica.com" title="">contatti</a>
+
+        </div>
