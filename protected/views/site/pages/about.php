8f30aece0c073dd7cb3265ecd9968aee3e422002
mystaimusic <mystaimusic@gmail.com>
2014-02-18 21:54:23 +0100

    aggiornamento testo

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,12 +1,81 @@
-<?php
-/* @var $this SiteController */
+<div class="container darkbg">
+  <div id="headercont">
 
-$this->pageTitle=Yii::app()->name . ' - About';
-$this->breadcrumbs=array(
-	'About',
-);
-?>
+
+
+<div id="mod_mainsearch">
+<div class="header-answer">What makes your body move...and your heart beat?</div>
+<div class="header-form">
+<input type="text" class='search_input'/><button type="button" class='search_button'>Go</button>
+</div><!--headerform-->
+</div><!--mod_mainsearch-->
+
+
+
+
+
+  </div><!--headercont-->
+</div><!--container-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container">
+
+<div id="maincont" class="clearfix">
+	
+    
+<div id="mainleft">
+<div class="mainleft_text">
 <h1>About</h1>
+<h2>STAIMUSIC &egrave; un servizio di musica e video musicali in streaming.</h2>
+<p>E' un progetto nato nel 2013 per cercare di fare una selezione sonora puntando sulla  qualit&agrave; delle canzoni, sulla potenza delle parole e sulla ricercatezza delle raccolte. 
+</p><br />
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<h2>L'immersione in STAIMUSIC &egrave;:</h2>
+<p>
+- ritrovare una via musicale personalizzata <br />
+- scoprire generi e epoche<br />
+- trovare il proprio gusto musicale<br />
+- ascoltare musica che fa vibrare corpi<br />
+- godersi abbinamenti musicali rari<br />
+- entrare nell'essenza della musica<br />
+- lasciarsi trasportare attraverso storie, citt&agrave; concetti, illusioni vecchie e d'avanguardia.<br />
+<br /><br />
+
+Ozzy Osborne: &quot;Se la musica &egrave; troppo alta, tu sei troppo vecchio&quot;</p>
+<br />
+
+
+<h2>3 cose che ci contraddistingono:</h2>
+<p>creativit&agrave; - passione - armonia
+
+</p>
+
+
+</div><!--mainleft_text"-->		          
+</div><!--mainleft-->
+
+
+
+
+<div id="mainright">
+       
+</div><!--mainright-->
+
+
+
+
+
+</div><!--maincont-->
+</div><!--container-->
