7f6148536c8fdec3effa509a4548f0b26d0dec17
mystaimusic <mystaimusic@gmail.com>
2014-02-20 23:59:36 +0100

    nuova immagine

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -83,3 +83,157 @@ If you have business inquiries or other questions, please fill out the following
 </div><!-- form -->
 
 <?php endif; ?>
+
+
+
+
+
+<div class="container darkbg">
+  <div id="headercont">
+
+
+
+<div id="mod_mainsearch">
+<div class="header-answer">What makes your body move...and your heart beat?</div>
+<div class="header-form">
+<input type="text" class='search_input'/><button type="button" class='search_button'>Go</button>
+</div><!--headerform-->
+</div><!--mod_mainsearch-about-->
+
+
+
+
+
+  </div><!--headercont-->
+</div><!--container-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container">
+
+<div id="maincont" class="clearfix">
+	
+    
+<div id="mainleft">
+<div class="mainleft_text">
+<h1>About</h1>
+<p>
+Il nostro staff è dislocato in diversi paesi, contattaci per informazioni e se vuoi collaborare con noi<br />
+Contatto Spagna: barcelona@staimusic.com<br />
+Contatto U.S.A: newyork@staimusic.com<br />
+Contatto Belgio: bruxelles@staimusic.com<br />
+Contatto Italia: milano@staimusic.com<br /></p>
+
+
+
+
+
+
+
+<h2>Inviaci una mail attraverso la form</h2>
+<p>
+
+
+<?php if(Yii::app()->user->hasFlash('contact')): ?>
+
+<div class="flash-success">
+	<?php echo Yii::app()->user->getFlash('contact'); ?>
+</div>
+
+<?php else: ?>
+
+<p>
+If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+</p>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'contact-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name'); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'subject'); ?>
+		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'subject'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'body'); ?>
+		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'body'); ?>
+	</div>
+
+	<?php if(CCaptcha::checkRequirements()): ?>
+	<div class="row">
+		<?php echo $form->labelEx($model,'verifyCode'); ?>
+		<div>
+		<?php $this->widget('CCaptcha'); ?>
+		<?php echo $form->textField($model,'verifyCode'); ?>
+		</div>
+		<div class="hint">Please enter the letters as they are shown in the image above.
+		<br/>Letters are not case-sensitive.</div>
+		<?php echo $form->error($model,'verifyCode'); ?>
+	</div>
+	<?php endif; ?>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
+
+<?php endif; ?>
+
+</p>
+
+
+</div><!--mainleft_text"-->		          
+</div><!--mainleft-->
+
+
+
+
+<div id="mainright">
+       
+</div><!--mainright-->
+
+
+
+
+
+</div><!--maincont-->
+</div><!--container-->
