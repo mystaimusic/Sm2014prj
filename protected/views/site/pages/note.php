450dfe1da17a5342933b8ea88baae58362dce318
mystaimusic <mystaimusic@gmail.com>
2014-02-28 01:21:06 +0100

    note

---
+++ protected/views/site/pages/note.php
@@ -0,0 +1,110 @@
+<?php
+/* @var $this SiteController */
+/* @var $model ContactForm */
+/* @var $form CActiveForm */
+
+
+?>
+
+
+
+
+
+<div class="container darkbg">
+  <div id="headercont">
+
+
+
+<div id="mod_mainsearch">
+<div class="header-answer">What makes your body move...and your heart beat?</div>
+<div class="header-form">
+<input type="text" class='search_input'/><button type="button" class='search_button'>Go</button>
+</div><!--headerform-->
+</div><!--mod_mainsearch-about-->
+
+
+
+
+
+  </div><!--headercont-->
+</div><!--container-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container">
+
+<div id="maincont" class="clearfix">
+	
+    
+<div id="mainleft">
+<div class="mainleft_text">
+<h1>TERMINI E CONDIZIONI DI USO DEL SITO</h1>
+<p>La consultazione e l'accesso al sito www.staimusic.com ( il "Sito") sono soggetti alle normative vigenti e implicano l'accettazione dei Termini e delle Condizioni di seguito riportati ("Termini e Condizioni"), che prevalgono anche rispetto a ogni altro diverso accordo intervenuto o che si ritenga intervenuto tra il visitatore del Sito (il "Visitatore") e Staimusic. Staimuisc si riserva di modificare in qualsiasi momento e senza preavviso i Termini e Condizioni qui riportati I Visitatori sono invitati a controllare periodicamente questa sezione del Sito per prendere visione delle eventuali modifiche.
+</p><br /><br />
+
+<h2>Limitazioni di uso</h2>
+<p>
+Il Visitatore &egrave;  autorizzato ad utilizzare e pu&ograve; scaricare testi e immagini per uso personale, ad esclusione, quindi, di usi commerciali o professionali. Salvo ove diversamente indicato, i contenuti del Sito sono protetti dalle norme sul marchio e/o sul diritto d'autore e/o sulla propriet&agrave; intellettuale e non possono essere utilizzati se non in conformit&agrave; e nei limiti di quanto qui previsto. In particolare, non &egrave;  concesso al Visitatore il diritto di riprodurre, distribuire, modificare, riutilizzare, inoltrare il contenuto del Sito, o di una sua parte, senza l'autorizzazione scritta ed esplicita della Staimusic. La Staimusic non sar&agrave; responsabile della violazione di diritti di terzi commessa dai Visitatori o da terzi e conseguente ad un uso improprio dei contenuti del Sito.
+<br /><br /><br />
+
+
+
+<h2>Contenuti del Sito</h2>
+<p>Malgrado i contenuti del Sito siano stati elaborati e formulati con la massima cura, la Staimusic non garantisce n&eacute; gli stessi siano privi di imprecisioni, inesattezze o omissioni, n&eacute; di essere in grado di aggiornarli in tempo reale o di mantenerli sempre disponibili, riservandosi di apportare in qualsiasi momento cambiamenti, correzioni, migliorie ai contenuti del Sito, o di interromperne o sospenderne le funzionalit&agrave;. La Staimusic non assume pertanto responsabilit&agrave; conseguenti ad incompletezza, inesattezza o mancato aggiornamento delle informazioni contenute nel Sito, impossibilit&agrave; di accedere al Sito o di visualizzarne o scaricarne o utilizzarne correttamente i contenuti, n&eacute; per eventuali danni o virus che possano colpire l'attrezzatura informatica o altri beni del Visitatore in conseguenza dell'accesso, utilizzo o della navigazione nel Sito o del download di suo contenuti.
+</p><br /><br />
+
+
+
+<h2>Link a siti terzi</h2>
+<p>Il Sito potrebbe contenere collegamenti ipertestuali a siti web di terzi, che possono offrire informazioni utili ai visitatori e sul cui contenuto la Staimusic non ha alcun controllo. Poich&egrave;  questi Termini e Condizioni non si applicano a tali siti e alle modalit&agrave; di utilizzo e accesso agli stessi, il collegamento a siti terzi avviene sotto l'esclusiva responsabilit&agrave; del Visitatore e la Staimusic non potr&agrave; in alcun modo essere ritenuta responsabile per l'esattezza, la sicurezza o l'affidabilit&agrave; di contenuti, prodotti o servizi disponibili su tali siti. 
+</p><br /><br />
+
+
+
+
+<h2>Dati personali e dati di navigazione</h2>
+<p>L'accesso e l'uso del Sito non comportano l'acquisizione di dati personali del Visitatore salvo ove espressamente segnalato (Area Riservata). In tali casi, il Visitatore sar&agrave; tenuto a prendere visione dell'informativa e -se del caso- ad esprimere il proprio consenso secondo quanto previsto dal D. Lgs. 30 giugno 2003 n. 196. Tuttavia, le procedure e il sistema informatico preposto al funzionamento del Sito potranno acquisire, nel corso del loro normale funzionamento, alcuni dati la cui trasmissione &egrave;  implicita nell'uso e protocolli di comunicazione Internet. Queste informazioni non sono raccolte per essere associate a interessati identificati ma per loro stessa natura potrebbero, attraverso elaborazioni ed associazioni con altri dati detenuti dalla Staimusic o da terzi, permettere l'identificazione del Visitatore. In questa categoria di dati rientrano gli indirizzi IP o i nomi a dominio dei computer utilizzati dagli utenti che si connettono al Sito, gli indirizzi in URI (Uniform Resource Identifier) delle risorse richieste, l'ora di richiesta, il metodo di richiesta, la dimensione dei file ottenuto in risposta, il codice numerico contrassegnante la risposta del server e altri parametri relativi al sistema operativo e all'ambiente informatico del Visitatore. Questi dati potranno essere utilizzati dalla Staimusic al solo fine di ricavarne informazioni assolutamente anonime e aggregate sull'uso del Sito allo scopo, ad es., di identificare le pagine o i contesti preferiti dagli utenti al fine di fornire servizi sempre pi&ugrave; adeguati e verificarne il corretto funzionamento. Solo in caso di violazioni o ipotetici reati informatici che coinvolgano il Sito tali dati potranno essere utilizzati per l'accertamento di responsabilit&agrave;. Segnaliamo infine che l'accesso al Sito potr&agrave; comportare la registrazione, sul disco fisso del dispositivo del Visitatore (computer, tablet, palmare o telefono) di "cookies". I "cookies" sono piccoli file di testo che i siti visitati dall'utente inviano al suo terminale (solitamente al browser), dove vengono memorizzati per essere poi ritrasmessi agli stessi siti alla successiva visita del medesimo utente. Le informazioni salvate nei cookies resteranno anonime e non consentiranno l'identificazione del Visitatore. Tuttavia, il Visitatore potr&agrave; in ogni momento richiedere la disattivazione dei cookies modificando le impostazioni del suo browser. La maggior parte dei browsers accetta i cookies automaticamente, ma &egrave;  possibile cancellare i cookies gi&agrave; registrati ed evitare la registrazione di nuovi cookies selezionando l'apposita opzione (eventualmente consultando le istruzioni del browser). 
+</p><br /><br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div><!--mainleft_text"-->		          
+</div><!--mainleft-->
+
+
+
+
+<div id="mainright">
+       
+</div><!--mainright-->
+
+
+
+
+
+</div><!--maincont-->
+</div><!--container-->
