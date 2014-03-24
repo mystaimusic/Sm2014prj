470fc1dbb666d4be786d133913fd8341923fd770
gabriele <mystaimusic@gmail.com>
2014-03-24 18:40:21 +0100

    added utilities class

---
+++ protected/components/Utilities.php
@@ -0,0 +1,44 @@
+<?php
+
+class Utilities{
+
+	public static function replaceDefaultImage($_imgInput)
+	{
+		$imagePath = "images/stai-music.jpg";
+		//echo Yii::trace(CVarDumper::dumpAsString($_imgInput),'vardump');
+		if(file_exists ( $_imgInput )){
+			$imagePath = $_imgInput;
+		}
+		$imagePath2 = Yii::app()->request->baseUrl."/".$imagePath;
+		//echo Yii::trace(CVarDumper::dumpAsString($imagePath2),'vardump');
+		//echo Yii::trace(CVarDumper::dumpAsString(Yii::app()->request->baseUrl),'vardump');
+		return $imagePath;
+	}
+	
+	public static function replaceDefaultImageArray($_imgInputArray)
+	{
+		foreach( $_imgInputArray as $_imgInput){
+			$_tmpImagePath = Utilities::replaceDefaultImage($_imgInput['IMAGEPATH']);
+		}
+	}
+	
+	public static function getTagUrl($title,$id)
+	{
+		$tagUrl = Utilities::getCustomUrl("musica/tag/",$title,$id);
+		//echo Yii::trace(CVarDumper::dumpAsString($tagUrl),'vardump');
+		return $tagUrl;
+	}
+	
+	public static function getPlaylistUrl($title,$id)
+	{
+		return Utilities::getCustomUrl("musica/playlist/",$title,$id);
+	}
+	
+	public static function getCustomUrl($prefix,$title,$id)
+	{
+		$customTag = Yii::app()->createUrl($prefix,array('title'=>$title,'id'=>$id));
+		//echo Yii::trace(CVarDumper::dumpAsString($customTag),'vardump');
+		return $customTag;
+	}
+
+}
