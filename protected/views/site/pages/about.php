4829f9f1d0d2cc288c8ecc6717756b635f92db6a
mystaimusic <mystaimusic@gmail.com>
2014-08-05 16:04:59 +0200

    Update about_page multilanguage

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -24,14 +24,14 @@
 <h2><?php $transl = Yii::t('msg','STAIMUSIC is a streaming music service');echo $transl; ?></h2>
 <p><?php $transl = Yii::t('msg','It’s a project born in Milan (Italy) in 2013; our main goal is to try to make a sound selection focused on the quality of the songs, based on the power of the words and founded on the refinement of the collections. We want to give you the opportunity to listen, to watch and to discover the music.');echo $transl; ?> 
 </p><br />
-<h2><?php $transl = Yii::t('msg','Diving in STAIMUSIC is: ');echo $transl; ?></h2>
+<h2><?php $transl = Yii::t('msg','Diving in STAIMUSIC is:');echo $transl; ?></h2>
 <p>- <?php $transl = Yii::t('msg','to find a personalized way');echo $transl; ?><br />
 - <?php $transl = Yii::t('msg','to discover genres and ages');echo $transl; ?><br />
 - <?php $transl = Yii::t('msg','to find out your personal music taste');echo $transl; ?><br />
 - <?php $transl = Yii::t('msg','to listen to the music makes your body vibrate');echo $transl; ?><br />
 - <?php $transl = Yii::t('msg','to enjoy strange music combinations');echo $transl; ?><br />
 - <?php $transl = Yii::t('msg','to enter in the real musical essence');echo $transl; ?><br /> 
-- <?php $transl = Yii::t('msg','to get you carry through stories, cities, concepts, old and avant-garde illusions ');echo $transl; ?><br /><br />
+- <?php $transl = Yii::t('msg','to get you carry through stories, cities, concepts, old and avant-garde illusions');echo $transl; ?><br /><br />
 <h2><?php $transl = Yii::t('msg','3 things that belong to us:');echo $transl; ?></h2>
 <p>- <?php $transl = Yii::t('msg','creativity');echo $transl; ?><br />
 - <?php $transl = Yii::t('msg','passion');echo $transl; ?><br />
