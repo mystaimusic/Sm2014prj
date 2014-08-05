45a6666a4722eba6a0aa9470d170067f35f490c1
mystaimusic <mystaimusic@gmail.com>
2014-08-05 15:29:55 +0200

    Update about_page multilanguage

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -20,28 +20,22 @@
     
 <div id="mainleft">
 <div class="mainleft_text">
-<p> <?php $transl = Yii::t('msg','Ozzy Osbourne: If it\'s too loud, you\'re too old');echo $transl; ?></div></p>
-<h2><?php $transl = Yii::t('msg','STAIMUSIC is a streaming music service that want to give you the opportunity to listen, to watch and to discover the music.');echo $transl; ?></div></h2><br />
-<p><?php $transl = Yii::t('msg','It’s a project born in Milan (Italy) 2013; his main goal is to try to make a sound selection focused on the songs quality, based on the words power and founded on the refinement of the collections');echo $transl; ?></div> 
-</p><br />
-<h2><?php $transl = Yii::t('msg','Diving in STAIMUSIC is:');echo $transl; ?></div></h2><br />
-<p><?php $transl = Yii::t('msg','- to find a personalized way/r/n
-- to discover genres and ages
-- to discover your personal music taste
-- to listen to the music makes your body vibrate
-- to enjoy strange music combinations
-- to enter in the real musical essence 
-- to get you carry through stories, cities, concepts, old and avant-garde illusions 
-');echo $transl; ?></div><br />
-
-<br /><br />
-</p>
-<br />
-
-
-<h2>3 cose che ci contraddistingono:</h2>
-<p>creativit&agrave; - passione - armonia
-
+<p> <?php $transl = Yii::t('msg','Ozzy Osbourne: If it\'s too loud, you\'re too old');echo $transl; ?></p><br><br />
+<h2><?php $transl = Yii::t('msg','STAIMUSIC is a streaming music service');echo $transl; ?></h2>
+<p><?php $transl = Yii::t('msg','It’s a project born in Milan (Italy) in 2013; our main goal is to try to make a sound selection focused on the quality of the songs, based on the power of the words and founded on the refinement of the collections. We want to give you the opportunity to listen, to watch and to discover the music.');echo $transl; ?> 
+</p><br><br />
+<h2><?php $transl = Yii::t('msg','Diving in STAIMUSIC is:');echo $transl; ?></h2>
+<p>- <?php $transl = Yii::t('msg','to find a personalized way');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','to discover genres and ages');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','to find out your personal music taste');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','to listen to the music makes your body vibrate');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','to enjoy strange music combinations');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','to enter in the real musical essence');echo $transl; ?><br /> 
+- <?php $transl = Yii::t('msg','to get you carry through stories, cities, concepts, old and avant-garde illusions ');echo $transl; ?><br /><br />
+<h2><?php $transl = Yii::t('msg','3 things that belong to us:');echo $transl; ?></h2>
+<p>- <?php $transl = Yii::t('msg','creativity');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','passion');echo $transl; ?><br />
+- <?php $transl = Yii::t('msg','enthusiasm');echo $transl; ?><br />
 </p>
 
 
