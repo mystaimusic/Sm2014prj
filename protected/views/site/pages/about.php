0fc13cdff71289205d3e5fa3e9928396843d1db7
mystaimusic <mystaimusic@gmail.com>
2014-08-05 13:17:29 +0200

    Update about multilanguage

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -20,21 +20,19 @@
     
 <div id="mainleft">
 <div class="mainleft_text">
-<p> <?php Yii::t('msg','Ozzy Osbourne: If it\'s too loud, you\'re too old');<br /></p>
-<h2><?php Yii::t('msg','STAIMUSIC is a streaming music service that want to give you the opportunity to listen, to watch and to discover the music.'); ></h2><br />
-<p><?php Yii::t('msg','It’s a project born in Milan (Italy) 2013; his main goal is to try to make a sound selection focused on the songs quality, based on the words power and founded on the refinement of the collections');?> 
+<p> <?php $transl = Yii::t('msg',Ozzy Osbourne: If it\'s too loud, you\'re too old');echo $transl; ?></div></p>
+<h2><?php $transl = Yii::t('msg','STAIMUSIC is a streaming music service that want to give you the opportunity to listen, to watch and to discover the music.');echo $transl; ?></div></h2><br />
+<p><?php $transl = Yii::t('msg','It’s a project born in Milan (Italy) 2013; his main goal is to try to make a sound selection focused on the songs quality, based on the words power and founded on the refinement of the collections');echo $transl; ?></div> 
 </p><br />
-<h2><?php Yii::t('msg','Diving in STAIMUSIC is:')></h2><br />
-<p>
-- <?php Yii::t('msg','
-- to find a personalized way/r/n
+<h2><?php $transl = Yii::t('msg','Diving in STAIMUSIC is:');echo $transl; ?></div></h2><br />
+<p><?php $transl = Yii::t('msg','- to find a personalized way/r/n
 - to discover genres and ages
 - to discover your personal music taste
 - to listen to the music makes your body vibrate
 - to enjoy strange music combinations
 - to enter in the real musical essence 
 - to get you carry through stories, cities, concepts, old and avant-garde illusions 
-');?><br />
+');echo $transl; ?></div><br />
 
 <br /><br />
 </p>
