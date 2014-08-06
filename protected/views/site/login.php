0684a76eb79327953e6ec218d98186ee3664d1fe
mystaimusic <mystaimusic@gmail.com>
2014-08-06 14:50:32 +0200

    Update multilanguage

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -11,7 +11,7 @@ $this->breadcrumbs=array(
 
 <h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
+<p><?php echo Yii::t('msg','Please fill out the following form with your login credentials')?>:</p>
 
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -22,7 +22,7 @@ $this->breadcrumbs=array(
 	),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note"><?php echo Yii::t('msg','Fields with')?><span class="required">*</span><?php echo Yii::t('msg','are required')?></p>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'username'); ?>
